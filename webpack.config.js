e39b5ba387d2c6ed0c40da74cddbf311f69e4796
jiwang <jiwang@alibaba-inc.com>
2016-01-19 22:47:39 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {  
+  entry: './src/index.ts',
+  output: {
+    filename: './dist/build.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+};
